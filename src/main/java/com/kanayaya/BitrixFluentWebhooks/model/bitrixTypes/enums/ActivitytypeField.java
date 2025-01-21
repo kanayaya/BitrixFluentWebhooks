08c10232049c317f77d1cf5434791382d1d63976
kanayaya <kanayaya@mail.ru>
2025-01-21 15:57:41 +0800

    add Activitytype

--- src/main/java/com/kanayaya/BitrixFluentWebhooks/model/bitrixTypes/enums/ActivitytypeField.java
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/bitrixTypes/enums/ActivitytypeField.java
@@ -1,4 +1,11 @@
 package com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.enums;
 
-public class ActivitytypeField {
+import com.kanayaya.BitrixFluentWebhooks.model.Field;
+import com.kanayaya.BitrixFluentWebhooks.model.enums.Activitytype;
+import com.kanayaya.BitrixFluentWebhooks.model.tables.Activity;
+
+public class ActivitytypeField extends Field<Activity, Activitytype> {
+    public ActivitytypeField(String name) {
+        super(name, Activitytype::getId, s -> Activitytype.getById(Integer.parseInt(s)));
+    }
 }
--- src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Activitytype.java
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Activitytype.java
@@ -1,5 +1,9 @@
 package com.kanayaya.BitrixFluentWebhooks.model.enums;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Activitytype {
     ACTIVITYTYPE_0(0, ""),
     MEETING(1, "Встреча"),
@@ -15,6 +19,19 @@ public enum Activitytype {
     Activitytype(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static final Map<Integer, Activitytype> byId = Arrays.stream(values()).collect(Collectors.toMap(a -> a.id, a->a));
+    public static Activitytype getById(Integer id) {
+        return byId.get(id);
     }
 }
 
