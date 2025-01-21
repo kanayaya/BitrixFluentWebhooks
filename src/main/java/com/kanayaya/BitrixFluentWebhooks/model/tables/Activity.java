68c79fbf9eba15495f34658440ee6976964f15f1
kanayaya <kanayaya@mail.ru>
2025-01-21 15:46:14 +0800

    add Activity

---
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/tables/Activity.java
@@ -0,0 +1,139 @@
+package com.kanayaya.BitrixFluentWebhooks.model.tables;
+
+import com.kanayaya.BitrixFluentWebhooks.model.Field;
+import com.kanayaya.BitrixFluentWebhooks.model.Table;
+import com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.*;
+import com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.entities.UserField;
+import com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.enums.CountryField;
+import com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.enums.YNField;
+
+import java.time.OffsetDateTime;
+
+import static com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.SerializerUtils.*;
+
+public class Activity extends Table {
+    /**
+    * ID
+    */
+public static final Field<Activity, Integer> ID = new Field<>("ID", INTEGER_DESERIALIZER);
+/**
+    * ID владельца
+    */
+public static final Field<Activity, Integer> OWNER_ID = new Field<>("OWNER_ID", INTEGER_DESERIALIZER);
+/**
+    * ID провайдера
+    */
+public static final StringField<Activity> PROVIDER_ID = new StringField<>("PROVIDER_ID");
+/**
+    * Тип провайдера
+    */
+public static final StringField<Activity> PROVIDER_TYPE_ID = new StringField<>("PROVIDER_TYPE_ID");
+/**
+    * Тип коннектора
+    */
+public static final StringField<Activity> PROVIDER_GROUP_ID = new StringField<>("PROVIDER_GROUP_ID");
+/**
+    * ID связанной с делом сущности
+    */
+public static final Field<Activity, Integer> ASSOCIATED_ENTITY_ID = new Field<>("ASSOCIATED_ENTITY_ID", INTEGER_DESERIALIZER);
+/**
+    * Тема
+    */
+public static final StringField<Activity> SUBJECT = new StringField<>("SUBJECT");
+/**
+    * Начало
+    */
+public static final DateField<Activity, OffsetDateTime> START_TIME = new DateField<>("START_TIME", DateField.DateLevel.OFFSET_DATE_TIME_LEVEL);
+/**
+    * Срок
+    */
+public static final DateField<Activity, OffsetDateTime> END_TIME = new DateField<>("END_TIME", DateField.DateLevel.OFFSET_DATE_TIME_LEVEL);
+/**
+    * Срок исполнения
+    */
+public static final DateField<Activity, OffsetDateTime> DEADLINE = new DateField<>("DEADLINE", DateField.DateLevel.OFFSET_DATE_TIME_LEVEL);
+/**
+    * Выполнено
+    */
+public static final YNField<Activity> COMPLETED = new YNField<>("COMPLETED");
+/**
+    * Ответственный
+    */
+public static final UserField<Activity> RESPONSIBLE_ID = new UserField<>("RESPONSIBLE_ID");
+/**
+    * Параметр уведомления
+    */
+public static final Field<Activity, Integer> NOTIFY_VALUE = new Field<>("NOTIFY_VALUE", INTEGER_DESERIALIZER);
+/**
+    * Описание
+    */
+public static final StringField<Activity> DESCRIPTION = new StringField<>("DESCRIPTION");
+/**
+    * Место
+    */
+public static final StringField<Activity> LOCATION = new StringField<>("LOCATION");
+/**
+    * Дата создания
+    */
+public static final DateField<Activity, OffsetDateTime> CREATED = new DateField<>("CREATED", DateField.DateLevel.OFFSET_DATE_TIME_LEVEL);
+/**
+    * Создал
+    */
+public static final UserField<Activity> AUTHOR_ID = new UserField<>("AUTHOR_ID");
+/**
+    * Дата изменения
+    */
+public static final DateField<Activity, OffsetDateTime> LAST_UPDATED = new DateField<>("LAST_UPDATED", DateField.DateLevel.OFFSET_DATE_TIME_LEVEL);
+/**
+    * Изменил
+    */
+public static final UserField<Activity> EDITOR_ID = new UserField<>("EDITOR_ID");
+/**
+    * Внешний код
+    */
+public static final StringField<Activity> ORIGIN_ID = new StringField<>("ORIGIN_ID");
+/**
+    * Внешний источник
+    */
+public static final StringField<Activity> ORIGINATOR_ID = new StringField<>("ORIGINATOR_ID");
+/**
+    * RESULT_STATUS
+    */
+public static final Field<Activity, Integer> RESULT_STATUS = new Field<>("RESULT_STATUS", INTEGER_DESERIALIZER);
+/**
+    * RESULT_STREAM
+    */
+public static final Field<Activity, Integer> RESULT_STREAM = new Field<>("RESULT_STREAM", INTEGER_DESERIALIZER);
+/**
+    * RESULT_SOURCE_ID
+    */
+public static final StringField<Activity> RESULT_SOURCE_ID = new StringField<>("RESULT_SOURCE_ID");
+/**
+    * Данные провайдера
+    */
+public static final StringField<Activity> PROVIDER_DATA = new StringField<>("PROVIDER_DATA");
+/**
+    * RESULT_MARK
+    */
+public static final Field<Activity, Integer> RESULT_MARK = new Field<>("RESULT_MARK", INTEGER_DESERIALIZER);
+/**
+    * RESULT_VALUE
+    */
+public static final Field<Activity, Double> RESULT_VALUE = new Field<>("RESULT_VALUE", DOUBLE_DESERIALIZER);
+/**
+    * RESULT_SUM
+    */
+public static final Field<Activity, Double> RESULT_SUM = new Field<>("RESULT_SUM", DOUBLE_DESERIALIZER);
+/**
+    * RESULT_CURRENCY_ID
+    */
+public static final StringField<Activity> RESULT_CURRENCY_ID = new StringField<>("RESULT_CURRENCY_ID");
+/**
+    * Автозаполнение
+    */
+public static final Field<Activity, Integer> AUTOCOMPLETE_RULE = new Field<>("AUTOCOMPLETE_RULE", INTEGER_DESERIALIZER);
+/**
+    * IS_INCOMING_CHANNEL
+    */
+public static final YNField<Activity> IS_INCOMING_CHANNEL = new YNField<>("IS_INCOMING_CHANNEL");
+}
