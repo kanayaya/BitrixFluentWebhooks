bb25a3b5dbc666fe416ea95c8f34aeb15905bcc9
kanayaya <kanayaya@mail.ru>
2025-01-21 15:45:57 +0800

    add Activitypriority

--- src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Activitypriority.java
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Activitypriority.java
@@ -1,5 +1,9 @@
 package com.kanayaya.BitrixFluentWebhooks.model.enums;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Activitypriority {
     EMPTY(0, ""),
     LOW(1, "низкая"),
@@ -12,6 +16,19 @@ public enum Activitypriority {
     Activitypriority(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static Map<Integer, Activitypriority> byId = Arrays.stream(values()).collect(Collectors.toMap(a -> a.id, a->a));
+    public static Activitypriority getById(Integer id) {
+        return byId.get(id);
     }
 }
 
