d6e7111a5c09417d2861aa247616a7fcfffdf12f
kanayaya <kanayaya@mail.ru>
2025-01-21 16:02:59 +0800

    add Contenttype

---
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/bitrixTypes/enums/ContenttypeField.java
@@ -0,0 +1,13 @@
+package com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.enums;
+
+import com.kanayaya.BitrixFluentWebhooks.model.Field;
+import com.kanayaya.BitrixFluentWebhooks.model.enums.Contenttype;
+import com.kanayaya.BitrixFluentWebhooks.model.tables.Activity;
+
+import java.util.function.Function;
+
+public class ContenttypeField extends Field<Activity, Contenttype> {
+    public ContenttypeField(String name, Function<String, Contenttype> deserializer) {
+        super(name, Contenttype::getId, s -> Contenttype.getById(Integer.parseInt(s)));
+    }
+}
--- src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Contenttype.java
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/enums/Contenttype.java
@@ -1,5 +1,9 @@
 package com.kanayaya.BitrixFluentWebhooks.model.enums;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Contenttype {
     EMPTY(0, ""),
     PLAIN_TEXT(1, "Plain text"),
@@ -12,6 +16,18 @@ public enum Contenttype {
     Contenttype(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static final Map<Integer, Contenttype> byId = Arrays.stream(values()).collect(Collectors.toMap(a -> a.id, a->a));
+    public static Contenttype getById(Integer id) {
+        return byId.get(id);
     }
 }
 
