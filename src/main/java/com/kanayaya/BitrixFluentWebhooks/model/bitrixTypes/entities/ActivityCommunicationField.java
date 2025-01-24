624b9f4d1b3230b1f6d227224878fe8b213e3152
kanayaya <kanayaya@mail.ru>
2025-01-24 10:42:24 +0800

    rename class

--- src/main/java/com/kanayaya/BitrixFluentWebhooks/model/bitrixTypes/entities/ActivityCommunicationField.java
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/model/bitrixTypes/entities/ActivityCommunicationField.java
@@ -3,10 +3,8 @@ package com.kanayaya.BitrixFluentWebhooks.model.bitrixTypes.entities;
 import com.kanayaya.BitrixFluentWebhooks.model.Table;
 import com.kanayaya.BitrixFluentWebhooks.model.pojo.idable.ActivityCommunicationEntity;
 
-import java.util.function.Function;
-
-public class ActivityCommunicationEntityField<TABLE extends Table> extends EntityField<TABLE, ActivityCommunicationEntity> {
-    public ActivityCommunicationEntityField(String name) {
+public class ActivityCommunicationField<TABLE extends Table> extends EntityField<TABLE, ActivityCommunicationEntity> {
+    public ActivityCommunicationField(String name) {
         super(name, s -> new ActivityCommunicationEntity(Integer.parseInt(s)));
     }
 }
