2b80c31a63b0f0ae9f09572802385845e19535ef
kanayaya <kanayaya@mail.ru>
2025-03-29 10:02:15 +0800

    add package doc

---
+++ src/main/java/com/kanayaya/BitrixFluentWebhooks/api/enums/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Помещайте сюда перечисления, содержащие информацию по аргументам,
+ * имеющим ограниченное количество вариантов.<br>
+ *
+ * Примером может служить {@link com.kanayaya.BitrixFluentWebhooks.api.enums.Order}
+ */
+package com.kanayaya.BitrixFluentWebhooks.api.enums;
